6a0ab4d9ce60a284e4d531263f44ef651381d877
Vladislav Nikitin <dushekrad@gmail.com>
2020-06-02 13:45:36 +0400

    chore(jimp): remove unused variables; Rename a file

--- jimp.js
+++ jimp.js
@@ -1,8 +1,8 @@
 const Jimp = require("jimp")
 
-var images = ["./images/container.png", "./images/photo1.jpeg", "./images/download.jpeg"]
+const images = ["./images/container.png", "./images/photo1.jpeg", "./images/download.jpeg"]
 
-var jimps = []
+const jimps = []
 
 for (let i = 0; i < images.length; i++) {
     jimps.push(Jimp.read(images[i]))
@@ -24,15 +24,7 @@ async function someFunc(someJimps) {
     for (let image of data) {
         widths.push(image.getWidth())
         heights.push(image.getHeight())
-    }
 
-    let composedWidth = 0
-    widths.forEach((pixels) => composedWidth += pixels)
-
-    let composedHeight = 0
-    heights.forEach((pixels) => composedHeight += pixels)
-
-    for (let image of data) {
         if (image.getHeight() > maxHeight) {
             maxHeight = image.getHeight()
         }
